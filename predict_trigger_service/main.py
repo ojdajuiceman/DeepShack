5beb4e8a84ee281c7725aa47e932b30de9c9a915
ojasklowski <ojasklowski@hioscar.com>
2019-05-11 20:58:03 -0400

    DEEP-83: (Again) Predict trigger publishes to triggerSMS topic

--- predict_trigger_service/main.py
+++ predict_trigger_service/main.py
@@ -5,6 +5,17 @@ import boto3
 
 logging.getLogger().setLevel(logging.INFO)
 
+SMS_TOPIC_ARN = 'arn:aws:sns:us-east-1:245636212397:triggerSMS'
+
+def publish_message(message, topic_arn):
+    """Publishes a message to SNS topic"""
+    sns = boto3.client('sns')
+    response = sns.publish(
+        TopicArn=topic_arn,
+        Message=json.dumps({'default': json.dumps(message)}),
+        MessageStructure='json'
+    )
+    logging.info(response) 
 
 def predict_trigger_handler(event, context):
     """Subscribes to triggerPredict SNS topic and triggers"""
@@ -12,7 +23,15 @@ def predict_trigger_handler(event, context):
     inbound_message = json.loads(event['Records'][0]['Sns']['Message'])
     filename = inbound_message['filename']
     phone_number = inbound_message['phone_number']
-
+    
+    outbound_message = {'filename': filename, 'phone_number': phone_number}
+    outbound_message['body'] = (
+                "We are now spinning up an ECS instance. "
+                "This machine will count the people in the Shack line. "
+                "This could take up to a minute."
+    )
+    publish_message(outbound_message, SMS_TOPIC_ARN)
+    
     ecs = boto3.client('ecs')
     response = ecs.run_task(
         cluster='cluster2',
